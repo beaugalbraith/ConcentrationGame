6cff6b9e1304d9f689e131a4e981ebd6da6821e9
Beau <beaugalbraith@gmail.com>
2017-11-27 11:36:33 -0600

    Class 1

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var flipCount: Int = 0
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🤯", on: sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.blue
+            
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.white
+        }
+        flipCount += 1
+    }
 
 }
 
